aa3b5ea9e2e0cdee5f7a61a494e67e3863514374
Rodrigo Abreu <rodrigo.everton@sempreceub.com>
2023-06-25 18:46:06 -0300

    resolvidos

--- arrays14.go
+++ arrays14.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	num1 := 0
+	num2 := 0
+	fmt.Print("Digite o primeiro índice: ")
+	fmt.Scan(&num1)
+	fmt.Print("Digite o segundo índice: ")
+	fmt.Scan(&num2)
+	n1 := slice[num1]
+	n2 := slice[num2]
+	slice[num1] = n2
+	slice[num2] = n1
+	for _, elementos := range slice {
+		fmt.Print(elementos, " ")
+	}
 }
